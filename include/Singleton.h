b081ec604c3bab3b242660b44f832cf8c4ec131f
Feng Jianwei <82906775@qq.com>
2021-12-14 01:19:42 +0800

    [modifier]fengjianwei [description] init workflows

---
+++ include/Singleton.h
@@ -0,0 +1,11 @@
+/**
+ * copyright 2021 The lightlog Authors
+ * Distributed under Apache-2.0
+**/
+
+#ifndef _SINGLETON_H_
+#define _SINGLETON_H_
+
+#define DefineSingle()
+
+#endif
---
+++ src/logmgr/logmgr.cpp
@@ -0,0 +1,5 @@
+#include <atomic>
+#include <iostream>
+ 
+using namespace std;
+
